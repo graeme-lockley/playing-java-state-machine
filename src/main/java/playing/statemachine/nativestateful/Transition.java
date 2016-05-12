57afca7be63bc65a2fa3eff4b631c4a78b95f342
Graeme Lockley <graemel@no9.co.za>
2016-05-12 06:17:31 +0100

    Made the fields package protected.

--- src/main/java/playing/statemachine/nativestateful/Transition.java
+++ src/main/java/playing/statemachine/nativestateful/Transition.java
@@ -1,12 +1,12 @@
 package playing.statemachine.nativestateful;
 
 class Transition<STATE, EVENT, RS> {
-    public final STATE fromState;
-    public final EVENT event;
-    public final STATE toState;
-    public final Action<RS> action;
+    final STATE fromState;
+    final EVENT event;
+    final STATE toState;
+    final Action<RS> action;
 
-    public Transition(STATE fromState, EVENT event, STATE toState, Action<RS> action) {
+    Transition(STATE fromState, EVENT event, STATE toState, Action<RS> action) {
         this.fromState = fromState;
         this.event = event;
         this.toState = toState;
